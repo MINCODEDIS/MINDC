a761c017ee258cb5b42d5170c53289b2aeefee6a
MINCODEDIS <66481016+MINCODEDIS@users.noreply.github.com>
2021-07-06 22:49:11 +0900

    Update server.cmd

--- server.cmd
+++ server.cmd
@@ -11,10 +11,12 @@ cls
 echo 1.윈도우11설치확인파일실행
 echo 2.테트리스
 echo 3.종료
+echo 4.마인크래프트 플러그인다운로드
 set /p aa=:
 if %aa%==1 goto 1
 if %aa%==2 goto 2
 if %aa%==3 goto 3
+if %aa%==4 goto 4
 
 echo error
 goto error
@@ -36,6 +38,11 @@ goto ho
 :3
 title 종료중
 exit
+
+:4
+echo 서버버킷이있는곳을선택해주세요!(server.properties)
+set /p ws=:
+echo %ws% 가 맞나요?
 
 :error
 exit
